9e66b5428a2ca271b46e414d3f669955a24f3d81
sawel24 <sawel.gnidenko@gmail.com>
2023-05-09 23:01:30 +0300

    Implement firebase connection

---
+++ lib/models/user/user_model.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_UserModel _$$_UserModelFromJson(Map<String, dynamic> json) => _$_UserModel(
+      id: json['id'] as String,
+      email: json['email'] as String,
+      assetUrl: json['assetUrl'] as String?,
+      workModelId: json['workModelId'] as String?,
+      isRecruiter: json['isRecruiter'] as bool?,
+    );
+
+Map<String, dynamic> _$$_UserModelToJson(_$_UserModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'email': instance.email,
+      'assetUrl': instance.assetUrl,
+      'workModelId': instance.workModelId,
+      'isRecruiter': instance.isRecruiter,
+    };
